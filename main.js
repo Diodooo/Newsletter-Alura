03f95004de68253e42b1084a69e386032ad7ed88
Diodooo <110136149+Diodooo@users.noreply.github.com>
2022-10-27 09:53:34 -0300

    Nesletter - 27/10/2022
    Corrigi um bug para telas menores

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ botao.addEventListener('mouseover', (event) => {
     console.log(input)
     if(input.value != "") return;
     const alvo = event.target.getAttribute('class');
-    if(alvo.includes('top-[150px]')){
-        event.target.classList.replace("top-[150px]", "top-[80px]");
+    if(alvo.includes('lg:top-[150px]')){
+        event.target.classList.replace("lg:top-[150px]", "lg:top-[80px]");
     } else {
-    event.target.classList.replace("top-[80px]", "top-[150px]");
+    event.target.classList.replace("lg:top-[80px]", "lg:top-[150px]");
     }
 });
